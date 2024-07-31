34a226dfbc08beac22c810c623e534de0784b29f
Edson-Emanoel <edson.emanoel2020@gmail.com>
2024-07-31 12:50:05 -0300

    Fiz fragments

---
+++ src/main/java/com/thymeleaf/thymeleaf/controllers/CadastroController.java
@@ -0,0 +1,12 @@
+package com.thymeleaf.thymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CadastroController {
+    @GetMapping("/cadastro")
+    public String cadastro(){
+        return "cadastro/index";
+    }
+}
